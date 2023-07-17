8e732c20a05cbd0852182728240d21b30618a6fc
AhegaoBurger <artur.waysman.2004@mail.ru>
2023-07-17 19:18:32 +0100

    Added data persistency, changed tasks to div instead of input

--- to_do_list/to_do.js
+++ to_do_list/to_do.js
@@ -1,24 +1,42 @@
+function saveTasks() {
+    const taskList = document.getElementById('taskList')
+    const taskDone = document.getElementById('taskDone')
+
+    const tasks = {
+        todo: taskList.innerHTML,
+        done: taskDone.innerHTML,
+    }
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+// This functions is for handling task completion
+function markTaskDone() {
+    const doneList = document.getElementById('taskDone')
+    doneList.appendChild(this.parentElement)
+    this.remove()
+    alert('Task Done')
+    saveTasks()
+}
+
 function addTask() {
     // get the task input element
     let taskInput = document.getElementById('tname')
     
-    // create a new input element
-    let newInput = document.createElement('input')
-    newInput.type = 'text'
-    newInput.value = taskInput.value
+    // create a new div element
+    let newInput = document.createElement('div')
+    newInput.textContent = taskInput.value
     
     // creating a mark done button element
-    let markTaskDone = document.createElement('button')
-    markTaskDone.type = 'button'
-    markTaskDone.innerHTML = 'Task Done'
-    markTaskDone.addEventListener('click' ,function() {
-        let doneList = 
-    })
+    let taskDoneButton = document.createElement('button')
+    taskDoneButton.type = 'button'
+    taskDoneButton.innerHTML = 'Task Done'
+    taskDoneButton.addEventListener('click', markTaskDone)
     
     // create a new paragraph and append the input to it
     let newP = document.createElement('p')
     newP.appendChild(newInput)
-    newP.appendChild(markTaskDone)
+    newP.appendChild(taskDoneButton)
     
     // get the task list div and append the new paragraph to it
     const taskList = document.getElementById('taskList')
@@ -28,27 +46,32 @@ function addTask() {
     taskInput.value = ''
     
     // Save the tasks so that when the user reloads the page, they will still be there
-    localStorage.setItem('tasks', taskList.innerHTML)
-
-    // alert('Task added to the list')
-}
-
-// This functions is for handling task completion
-function markTaskDone() {
-    const doneList = document.getElementById('taskDone')
-    doneList.appendChild(this.parentElement)
-    this.remove()
-    alert('Task Done')
+    saveTasks()
 }
 
 // This function handles loading the saved tasks
 function loadSavedTasks() {
-    const savedTasks = localStorage.getItem('tasks')
+    const savedTasks = JSON.parse(localStorage.getItem('tasks'))
+    
     if (savedTasks) {
-        taskList.innerHTML = savedTasks
+        const taskList = document.getElementById('taskList')
+        const taskDone = document.getElementById('taskDone')
+        
+        taskList.innerHTML = savedTasks.todo
+        taskDone.innerHTML = savedTasks.done
 
         // This will get all the Task Done buttons
+        const todoButtons = taskList.getElementsByTagName('button')
+        const doneButtons = taskDone.getElementsByTagName('button')
 
+        for (let i  = 0; i < todoButtons.length; i++) {
+            // then it adds the event listeners for each button
+            todoButtons[i].addEventListener('click', markTaskDone)
+        }
+
+        for (let i  = 0; i < doneButtons.length; i++) {
+            doneButtons[i].remove()
+        }
     }
 }
 
