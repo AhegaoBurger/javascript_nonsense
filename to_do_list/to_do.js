f256c2843090a2e34b4e771e161a2a041b788a41
AhegaoBurger <artur.waysman.2004@mail.ru>
2023-07-19 23:24:56 +0100

    Added the edit button functionality

--- to_do_list/to_do.js
+++ to_do_list/to_do.js
@@ -19,6 +19,49 @@ function markTaskDone() {
     saveTasks()
 }
 
+function saveEditedTask() {
+    // first we will get the txt and make it into a node
+    let taskText = this.parentElement.firstChild.value
+    let textNode = document.createTextNode(taskText)
+
+    let taskDiv = this.parentElement
+    taskDiv.innerHTML = ''
+    taskDiv.appendChild(textNode)
+
+    let taskDoneButton = document.createElement('button')
+    taskDoneButton.textContent = 'Task Done'
+    taskDoneButton.addEventListener('click', markTaskDone)
+    taskDiv.appendChild(taskDoneButton)
+
+    let taskEditButton = document.createElement('button')
+    taskEditButton.textContent = 'Edit Task'
+    taskEditButton.className = 'edit-button'
+    taskEditButton.addEventListener('click', editTask)
+    taskDiv.appendChild(taskEditButton)
+    
+    saveTasks()
+}
+
+function editTask() {
+    let taskText = this.parentElement.firstChild.textContent
+    let input = document.createElement('input')
+    input.type = 'text'
+    input.value = taskText
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+    saveButton.addEventListener('click', saveEditedTask)
+
+    let taskEditButton = this.parentElement.querySelector('.edit-button')
+    taskEditButton.style.display = 'none'
+
+    let taskDiv = this.parentElement
+    taskDiv.innerHTML = ''
+    taskDiv.appendChild(input)
+    taskDiv.appendChild(saveButton)
+    taskDiv.appendChild(taskEditButton)
+}
+
 function addTask() {
     // get the task input element
     let taskInput = document.getElementById('tname')
@@ -31,12 +74,21 @@ function addTask() {
     let taskDoneButton = document.createElement('button')
     taskDoneButton.type = 'button'
     taskDoneButton.innerHTML = 'Task Done'
+    taskDoneButton.className = 'done-button'
     taskDoneButton.addEventListener('click', markTaskDone)
+
+    // creating an edit buton element
+    let taskEditButton = document.createElement('button')
+    taskEditButton.type = 'button'
+    taskEditButton.innerHTML = 'Edit Task'
+    taskEditButton.className = 'edit-button'
+    taskEditButton.addEventListener('click', editTask)
     
     // create a new paragraph and append the input to it
     let newP = document.createElement('p')
     newP.appendChild(newInput)
     newP.appendChild(taskDoneButton)
+    newP.appendChild(taskEditButton)
     
     // get the task list div and append the new paragraph to it
     const taskList = document.getElementById('taskList')
@@ -61,16 +113,16 @@ function loadSavedTasks() {
         taskDone.innerHTML = savedTasks.done
 
         // This will get all the Task Done buttons
-        const todoButtons = taskList.getElementsByTagName('button')
-        const doneButtons = taskDone.getElementsByTagName('button')
+        const todoButtons = taskList.getElementsByClassName('done-button')
+        const editButtons = taskDone.getElementsByClassName('edit-button')
 
         for (let i  = 0; i < todoButtons.length; i++) {
             // then it adds the event listeners for each button
             todoButtons[i].addEventListener('click', markTaskDone)
         }
 
-        for (let i  = 0; i < doneButtons.length; i++) {
-            doneButtons[i].remove()
+        for (let i  = 0; i < editButtons.length; i++) {
+            editButtons[i].addEventListener('click', editTask)
         }
     }
 }
